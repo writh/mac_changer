b6418e58072ec8dd905dffe076c0b589f360ccb0
root <victoria.thursday@gmail.com>
2020-07-16 18:04:19 -0400

    fixed the name of mac_changer

---
+++ mac_changer.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python
+
+import subprocess
+
+interface = input("interface > ")
+new_mac = input("new MAC > ")
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
