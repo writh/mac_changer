93a052307b650d809ff1ad885ea6768b952620f3
root <victoria.thursday@gmail.com>
2020-07-17 17:13:11 -0400

    changed vars to call parsed values

--- mac_changer.py
+++ mac_changer.py
@@ -8,13 +8,13 @@ parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest="interface", help="Use this option to choose the interface to change its MAC address.")
 parser.add_option("-m", "--mac", dest="new_mac", help="Use this option to choose the desired new MAC address.")
 
-parser.parse_args()
+(options, arguments) = parser.parse_args()
 
-interface = raw_input("interface > ")
-new_mac = raw_input("new MAC > ")
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+subprocess.call(["ifconfig", interface, "hw","ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
