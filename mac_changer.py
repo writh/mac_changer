1cd5ba9941110835d4a0c9ca43b5c693ba9327c9
root <victoria.thursday@gmail.com>
2020-07-17 17:33:18 -0400

    created function change_mac

--- mac_changer.py
+++ mac_changer.py
@@ -3,6 +3,13 @@
 import subprocess
 import optparse
 
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw","ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Use this option to choose the interface to change its MAC address.")
@@ -10,11 +17,4 @@ parser.add_option("-m", "--mac", dest="new_mac", help="Use this option to choose
 
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw","ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(options.interface, options.new_mac)
