8d874889e207770565c92701dbdc29184d0e7720
root <victoria.thursday@gmail.com>
2020-08-03 19:30:02 -0400

    fixed error where options was not called

--- mac_changer.py
+++ mac_changer.py
@@ -8,16 +8,15 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Use this option to choose the interface to change its MAC address.")
     parser.add_option("-m", "--mac", dest="new_mac", help="Use this option to choose the desired new MAC address.")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error("[-] you forgot the interface, dummy")
+        parser.error("[-] you forgot the interface, dummy. --help might be a good plan...")
     elif not options.new_mac:
-        parser.error("[-] you forgot the mac, dummy")
+        parser.error("[-] you forgot the mac, dummy. --help might be a good plan...")
     return options
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw","ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
