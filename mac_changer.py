6d9540c24f1d6dd4a321d309791c69375b9ccaa9
root <victoria.thursday@gmail.com>
2020-08-03 19:15:29 -0400

    added error logic and statements

--- mac_changer.py
+++ mac_changer.py
@@ -9,6 +9,11 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Use this option to choose the interface to change its MAC address.")
     parser.add_option("-m", "--mac", dest="new_mac", help="Use this option to choose the desired new MAC address.")
     return parser.parse_args()
+    if not options.interface:
+        parser.error("[-] you forgot the interface, dummy")
+    elif not options.new_mac:
+        parser.error("[-] you forgot the mac, dummy")
+    return options
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -18,5 +23,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
